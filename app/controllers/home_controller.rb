879d8d090e9a27b5a6ab9542d9a1768a38262d52
wmzxc <opxlt61445@yahoo.co.jp>
2022-06-18 20:45:20 +0900

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, only: :index
-
+  #before_action :authenticate_user!, only: :index
+  
   def index
+
+  end  
+  
+  def index
+      
+      @users = User.all
+      @students = User.all
   end
 end
